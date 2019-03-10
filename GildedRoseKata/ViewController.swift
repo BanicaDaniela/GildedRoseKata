93d90883a83ceba9946cffb282e2f2c1e0d03977
2019_DEV_094 <oana.daniela.banica@gmail.com>
2019-03-10 13:53:01 +0100

    Initial Commit

---
+++ GildedRoseKata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GildedRoseKata
+//
+//  Created by Daniela Banica on 10/03/2019.
+//  Copyright © 2019 Daniela Banica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
